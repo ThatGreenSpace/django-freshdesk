7a1c21718f04e75b97a765039f1478e0bea0160d
Alberto Resco Perez <alberto.resco@gmail.com>
2014-07-24 00:21:37 +0800

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     os.system('python setup.py bdist_wheel upload')
     print('You probably want to also tag the version now:')
-    print('git tag -a %s -m "version %s"' % (version, version))
+    print('git tag -a %s -m "version %s"' % (freshdesk.__version__, freshdesk.__version__))
     print('git push --tags')
     sys.exit()
 
