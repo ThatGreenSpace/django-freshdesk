ffc0c8a318ddde40c7ae0951126814989111c2b5
Maxim Kukhtenkov <maxim.kht@gmail.com>
2017-03-08 07:07:08 -0800

    Add Django 1.10 to unit tests and fix tests

--- freshdesk/tests.py
+++ freshdesk/tests.py
@@ -15,7 +15,7 @@ class ViewsTestCase(django.test.TestCase):
         """Test with user not logged in"""
         response = self.client.get(reverse(views.authenticate))
         self.assertEqual(302, response.status_code)
-        self.assertEqual(
+        self.assertIn(
             response['Location'], r'http://testserver%s?next=/freshdesk/' % settings.LOGIN_URL)
 
     def test_user_logged_in(self):
